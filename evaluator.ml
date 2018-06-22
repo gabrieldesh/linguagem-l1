c948d6c00b6f8ad24f289c52361998dd73a3763b
Gabriel <gabrielpakulski@hotmail.com>
2018-06-22 15:43:30 -0300

    Atualizações recentes sobre o avavaliador

--- evaluator.ml
+++ evaluator.ml
@@ -44,7 +44,30 @@ and
      env = (variable * value) list
 	 
 	 
+	 
+let empty_env : env = []
 
+
+let remove_tuple var list =
+  List.filter (fun (k,_) -> k <> var) list 
+  
+  
+let update_env var v1 env : env = match env with
+	[] -> [(var,v1)]
+	| hd::tl ->
+		if (List.exists (fun (k,_) -> k = var) env)
+			then List.append (remove_tuple var env) [(var,v1)]
+			else List.append env [(var,v1)]
+
+		
+let rec lookup_environment var env : value = match env with
+	| [] -> raise Not_found
+	| (k,v)::tl ->
+		if (k = var)
+			then v
+			else lookup_environment var tl
+				
+				
 let rec isValue exp = 
 match exp with
 	  Vnum _ -> true
@@ -55,46 +78,67 @@ match exp with
 	| exp -> false
 	
 	
-let rec eval exp =
-	match exp with
-	Num n -> n
-	| Bool b -> b
-	| Bop(_,Raise,_) -> Raise
-	| Bop(op,v,Raise) when isValue(v) -> Raise
-	| Bop(op,e1,e2) when isValue(eval(e1)) && isValue(eval(e2)) ->
-		(match op with
-			  Sum -> eval(e1) + eval(e2)
-			| Diff -> eval(e1) - eval(e2)
-			| Mult -> eval(e1) * eval(e2)
-			| Div ->(match eval(e2) with
-					0 -> Raise
-					| _ -> eval(e1) / eval(e2)
-					)
-			| Eq -> eval(e1) == eval(e2)
-			| And -> eval(e1) && eval(e2)
-			| Or -> eval(e1) || eval(e2)
-			| NotEqual -> eval(e1) != eval(e2)
-			| Less -> eval(e1) < eval(e2)
-			| Greater -> eval(e1) > eval(e2)
-			| LessOrEqual -> eval(e1) <= eval(e2)
-			| GreaterOrEqual -> eval(e1) >= eval(e2)
-		)
-	(*Não tenho certeza se precisa dessas:  *)
-	| Bop(op,e1,e2) when eval(e1) == Raise -> Raise
-	| Bop(op,e1,e2) when eval(e2) and isValue(eval(e1)) == Raise -> Raise
-	(*Fim da incerteza hehe*)
+let rec eval (env:env) (exp : expr) : value =	match exp with
+	(* Valores *)
+	Num(n) -> VNum(n)
+	| Bool(b) -> VBool(b)
+
+	(* Operações *)
 	
-	| Not(e1) ->(match eval(e1) with
+	(* Operações binárias *)
+	(* O primeiro operando avalia para Raise *)
+	| Bop(op,e1,e2) when eval(env e1) == Raise -> Raise
+	(* O segundo operando avalia para Raise *)	
+	| Bop(op,e1,e2) when isValue(eval(env e1)) && eval(env e2) == Raise -> Raise
+	(* Nenhum dos operandos avalia para raise *)
+	| Bop(op,e1,e2) ->
+		let n1 = eval env e1 in
+		let n2 = eval env e2 in
+		(match op with
+			  Sum -> n1 + n2
+			| Diff -> n1 - n2
+			| Mult -> n1 * n2
+			| Div ->(match n2 with
+					0 -> Raise
+					| _ -> n1 / n2
+					)
+			| Eq -> n1 == n2
+			| And -> n1 && n2
+			| Or -> n1 || n2
+			| NotEqual -> n1 != n2
+			| Less -> n1 < n2
+			| Greater -> n1 > n2
+			| LessOrEqual -> n1 <= n2
+			| GreaterOrEqual -> n1 >= n2
+		)
+	(* Not *)
+	| Not(e1) ->(match eval(env e1) with
 				 Raise -> Raise
 				| _ -> !eval(e1)
 				)
 				
-	| If(e1,e2,e3) when eval(e1) == Raise -> Raise
-	| If(e1,e2,e3) when eval(e1) == true ->(match eval(e2) with
+				
+	(* If *)
+	| If(e1,e2,e3) when eval(env e1) == Raise -> Raise
+	| If(e1,e2,e3) when eval(env e1) == true ->(match eval(env e2) with
 											Raise -> Raise
-											| _ -> eval(e2)
+											| _ -> eval(env e2)
 											)
-	| If(e1,e2,e3) when eval(e1) == false ->(match eval(e3) with
+	| If(e1,e2,e3) when eval(env e1) == false ->(match eval(env e3) with
 											 Raise -> Raise
-											 | _ -> eval(e3)
+											 | _ -> eval(env e3)
 											 )
+											 
+	(* Variável *)
+	| Var(variable) -> lookup_environment variable environment
+	
+	(* Aplicação *)
+	| App(e1,e2) when eval(env e1) == Raise -> Raise
+	| App(e1,e2) when isValue(eval(env e1)) && (eval(env e2) == Raise) -> Raise
+	| App(e1,e2) -> 
+		let v1 = eval env e1 in
+		let v2 = eval env e2 in
+		(match v1,v2 with
+			Vclos(var,e,env), v -> eval (update_env var v env) e
+		|	Vrclos(f,x,e,enf), v -> eval (update_env f (Vrclos(f,x,e,env)) (update_env x v env)) e
+	(* Função - Lam *)
